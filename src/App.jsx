06a8ee31fa53d06e8293562c6d170be5e4dbac33
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-06-17 11:13:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Board from "./components/Board.jsx";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-green-900 flex items-center justify-center">
+        <Board />
+      </div>
+    
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Board.jsx
@@ -0,0 +1,27 @@
+function Board() {
+    const size = 8;
+    const board = [];
+
+    for (let row = 0; row < size; row++) {
+        const cells = [];
+
+        for (let col = 0; col < size; col++) {
+            const isB = (row + col) % 2 === 1;
+            cells.push(
+                <div
+                    key={row * 10 + col}
+                    className={`w-16 h-16 ${isB ? 'bg-gray-800' : 'bg-white'}`}
+                >
+                </div>
+            );
+        } board.push(...cells);
+    }
+
+    return (
+        <div className="grid grid-cols-8 border border-gray-700 mx-auto mt-10">
+            {board}
+        </div>
+    )
+}
+
+export default Board;
